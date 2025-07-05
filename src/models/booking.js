4ca16c8e4c079a183e895c5eb95e7298836486b2
MANISH6148 <manishbobburi6148@gmail.com>
2025-07-05 18:48:16 +0530

    feat(models): add Bookings model and migrate

---
+++ src/models/booking.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const { Enums } = require("../utils/common");
+const { BOOKED, CANCELLED, INITIATED, PENDING } = require(Enums.BOOKING_STATUS);
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init({
+    flightId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    status: DataTypes.ENUM(BOOKED, CANCELLED, INITIATED, PENDING),
+    noOfSeats: DataTypes.INTEGER,
+    totalCost: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
