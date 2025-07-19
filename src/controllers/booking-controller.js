d29da6fe6ea4e10a6695e38a93ea90db43a16418
MANISH6148 <manishbobburi6148@gmail.com>
2025-07-19 13:34:27 +0530

    feat(controller): add idempotency validation.

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,6 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const { SuccessResponse, ErrorResponse} = require("../utils/common");
 const { BookingService } = require("../services");
 
+const inMemDb = {};
+
 async function createBooking(req, res) {
     try {
         const booking = await BookingService.createBooking({
@@ -10,6 +12,7 @@ async function createBooking(req, res) {
             userId: req.body.userId,
             noOfSeats: req.body.noOfSeats,
         });
+
         SuccessResponse.data = booking;
         return res
                 .status(StatusCodes.CREATED)
@@ -17,6 +20,7 @@ async function createBooking(req, res) {
     } 
     catch(error) {
         ErrorResponse.error = error;
+
         return res
                 .status(error.statusCode)
                 .json(ErrorResponse);
@@ -25,11 +29,26 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
     try {
+        const idempotencyKey = req.headers["x-idempotency-key"];
+
+        if(!idempotencyKey) {
+            return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json({message: "idempotency key not found in incoming request headers"});
+        }
+
+        if(inMemDb[idempotencyKey]) {
+            return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json({message: "Cannot retry on successful payment"});
+        }
+
         const response = await BookingService.makePayment({
           bookingId: req.body.bookingId,
           userId: req.body.userId,
           totalCost: req.body.totalCost,
         });
+        inMemDb[idempotencyKey] = idempotencyKey;
         SuccessResponse.data = response;
         return res
                 .status(StatusCodes.CREATED)
@@ -37,7 +56,7 @@ async function makePayment(req, res) {
     } 
     catch(error) {
         ErrorResponse.error = error;
-
+        console.error(error);
         return res
                 .status(error.statusCode)
                 .json(ErrorResponse);
