d290f817075c4dda6aa344a9442cd9567f8c9c5e
MANISH6148 <manishbobburi6148@gmail.com>
2025-08-09 22:40:41 +0530

    feat(CORS): add CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const apiRoutes = require("./routes");
 const { ServerConfig, Queue } = require("./config");
 const CRON = require("./utils/common/cron-jobs");
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173"}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
