ae4e98ead76dfa85b7d06876126aab885da67c15
MANISH6148 <manishbobburi6148@gmail.com>
2025-08-19 23:19:43 +0530

    add PaymentService

--- src/services/index.js
+++ src/services/index.js
@@ -2,4 +2,5 @@ module.exports = {
     TicketService: require("./ticket-service"),
     BookingService: require("./booking-service"),
     PassengerService: require("./passenger-service"),
+    PaymentService: require("./payment-service"),
 }
