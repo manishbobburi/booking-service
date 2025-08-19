49cd715c9cf9926a4d9d0134520615dc95283a05
MANISH6148 <manishbobburi6148@gmail.com>
2025-08-19 23:18:46 +0530

    add PaymentController

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,4 +2,5 @@ module.exports = {
     BookingController: require("./booking-controller"),
     TicketController: require("./ticket-controller"),
     PassengerController: require("./passenger-controller"),
+    PaymentController: require("./payment-controller"),
 }
